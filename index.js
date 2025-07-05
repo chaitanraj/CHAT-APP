b15caecfe5101e07f90ce007b93e2061097cf48d
Chaitanya Raj <crsofficial7@gmail.com>
2025-07-06 03:28:30 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require("express")
+const http=require("http")
+const path=require("path")
+const {Server}=require("socket.io")
+
+const app=express();
+const server=http.createServer(app);
+const io=new Server(server)
+
+//Socket io
+// socket=client 
+io.on('connection',(socket)=>{
+    console.log("New user connected",socket.id)
+})
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/",(req,res)=>{
+    return res.sendFile("/public/index.html")
+})
+
+
+server.listen(9000,()=>console.log("Server started at 9000"))
